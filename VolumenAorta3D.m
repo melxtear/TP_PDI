99c0292e4205858677e3a026b5c5d7f7831724e6
Mel Almada <101682418+melxtear@users.noreply.github.com>
2025-10-29 16:49:54 -0300

    VolumenAorta3D (agregué algo minimo)
    Le agregué el parametro que recibe por derecha en la funcion CentroideInicial

--- VolumenAorta3D.m
+++ VolumenAorta3D.m
@@ -5,7 +5,7 @@ vol = nrrdread(filename);
 VolMarcado = zeros([size(vol),3]); %RGB
 [~, ~,numSlices] = size(vol);
 CentroidesSlice = zeros(numSlices,2); %Vector de centroides de cada Slice formato [x,y]
-CentroidesSlice(1,:) = CentroideInicial();
+%CentroidesSlice(1,:) = CentroideInicial(vol);
 
 for i = size(vol,3):-1:1  
     [VolMarcado(:,:,i,:), CentroidesSlice(i+1,:)] = SegmentacionSlice(CentroidesSlice(i,:), vol(:,:,i));
