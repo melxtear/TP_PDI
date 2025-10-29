0665293552f2fc80e2462caa75d1acdfc68734b7
Mel Almada <101682418+melxtear@users.noreply.github.com>
2025-10-29 16:46:07 -0300

    Funcion CentroideInicial
    Se tiene la imagen inicial, se selecciona en ella el centroide y se guarda la coordenada.

--- CentroideInicial.m
+++ CentroideInicial.m
@@ -1,6 +1,9 @@
-function [Centroide] = CentroideInicial()
+function [x, y] = CentroideInicial(vol)
 %Algoritmo de selección manual (se abre la imagen y permite seleccionar con
 %un click)
 
+imshow(vol(:,:,190), []);
+impixelinfo
+[x, y, ~] = impixel;
 
 end
