e238e44be6a45afb82f819b9f0d75238cbc2f19a
Mel Almada <101682418+melxtear@users.noreply.github.com>
2025-10-29 16:49:09 -0300

    Main para probar CentroideInicial y Segmentacion
    Es algo para probar, despues se puede borrar

--- untitled.m
+++ untitled.m
@@ -1,4 +1,4 @@
-vol = nrrdread("Dongyang\D1\D1.nrrd");
+%vol = nrrdread("Dongyang\D1\D1.nrrd");
 
 %{
 for k = size(vol,3):-1:1
@@ -7,6 +7,16 @@ for k = size(vol,3):-1:1
     pause(0.05);
 end
 %}
-imshow(vol(:,:,190),[])
-impixelinfo
-SegmentacionAorta3D([315, 257],vol(:,:,100))
+%imshow(vol(:,:,190),[])
+%impixelinfo
+%VolumenAorta3D([315, 257],vol(:,:,100))
+%VolumenAorta3D("Dongyang\D1\D1.nrrd")
+%SegmentacionAorta3D([343, 273],vol(:,:,190))
+
+for k = 100:200
+    mask1 = Segmentacion(vol(:,:,k));
+    %figure(1);
+    %imshow(vol(:,:,k), []);
+    figure(2);
+    imshow(mask1);
+end
