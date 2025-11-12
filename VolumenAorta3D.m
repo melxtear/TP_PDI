f439c6a812e1b5485412deb5f282c3416d0d2210
Isabella Forte <113397251+isabellaforte@users.noreply.github.com>
2025-11-12 16:05:04 -0300

    Update VolumenAorta3D.m

--- VolumenAorta3D.m
+++ VolumenAorta3D.m
@@ -1,7 +1,7 @@
-function [] = VolumenAorta3D(filename)
+function [] = VolumenAorta3D(filename, SliceInicial, SliceFinal)
 
 vol = nrrdread(filename);
-vol = vol(:,:,100:200,:);
+vol = vol(:,:,SliceInicial:SliceFinal,:);
 VolMarcado = zeros([size(vol),1]); %RGB
 [~, ~,numSlices] = size(vol);
 CentroidesSlice = zeros(numSlices,4); %Vector de centroides de cada Slice formato [x,y]
@@ -14,3 +14,4 @@ for i = 1:size(vol,3)
 end
 
 %volshow(VolMarcado);
+%volshow(VolMarcado, 'Colormap', [0 0 0; 1 0 0],'IsosurfaceValue',0.5);
