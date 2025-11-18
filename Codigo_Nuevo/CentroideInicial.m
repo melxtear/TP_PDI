054cad58153b929d9ac19cb128495750ca6596d6
Isabella Forte <113397251+isabellaforte@users.noreply.github.com>
2025-11-18 19:22:25 -0300

    Delete Codigo_Nuevo directory

--- Codigo_Nuevo/CentroideInicial.m
+++
@@ -1,9 +0,0 @@
-function [x, y] = CentroideInicial(vol)
-%Algoritmo de selección manual (se abre la imagen y permite seleccionar con
-%un click)
-
-imshow(vol(:,:,1), []);
-impixelinfo
-[x, y, ~] = ginput(1);
-
-end
--- Codigo_Nuevo/visualizaciondearchivos.m
+++
@@ -1,9 +0,0 @@
-vol = nrrdread("Dongyang\D6\D6.nrrd");
-
-for k = 1:size(vol,3)
-    imshow(vol(:,:,k),[])
-    hold on
-    title(sprintf('Slice %d', k));
-
-    pause(0.2)
-end
